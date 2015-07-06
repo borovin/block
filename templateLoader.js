b16cf5f4f96229161a0c2f48aa2e0abc906e2f1a
Andrew Borovin <a@borovin.com>
2015-07-06 23:37:00 +0300

    update template loader

--- templateLoader.js
+++ templateLoader.js
@@ -1,17 +1,16 @@
 define(function(require) {
-    //requirements
-    var amdLoader = require('bower_components/amd-loader/amd-loader');
 
-    var _ = require('bower_components/lodash/lodash');
+    var _ = require('bower_components/lodash/lodash.js');
 
-    return amdLoader('tpl', function(name, source, req, callback, errback, config) {
+    return {
+        translate: function(load) {
 
-        var options = {
-            variable: 'params'
-        };
+            var options = {
+                variable: 'params'
+            };
 
-        var content = 'define(function(require){return ' + _.template(source, options) + '})';
+            return 'define(function(require){return ' + _.template(load.source, options) + '})';
+        }
+    }
 
-        callback(content);
-    });
 });
