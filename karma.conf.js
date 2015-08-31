adbbca6f4f72550099fce5eb8b5bad0552430166
Andrew Borovin <a@borovin.com>
2015-09-01 00:53:57 +0300

    add coverage reporter

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function(config) {
             // source files, that you wanna generate coverage for
             // do not include tests or libraries
             // (these files will be instrumented by Istanbul)
-            'block.js': ['coverage']
+            'index.js': ['coverage']
         },
 
         coverageReporter: {
