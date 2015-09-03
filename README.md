88643d31b07d896bf4c1ecba068e80ec8f461c60
Andrew Borovin <a@borovin.com>
2015-09-04 01:28:37 +0300

    update docs

--- README.md
+++ README.md
@@ -1,14 +1,46 @@
-[![Build Status](https://travis-ci.org/borovin/block.svg?branch=master)](https://travis-ci.org/borovin/block) [![Coverage Status](https://coveralls.io/repos/borovin/block/badge.svg?branch=master&service=github)](https://coveralls.io/github/borovin/block?branch=master) [![Code Climate](https://codeclimate.com/github/borovin/block/badges/gpa.svg)](https://codeclimate.com/github/borovin/block)
-=====
+# [![Build Status](https://travis-ci.org/borovin/block.svg?branch=master)](https://travis-ci.org/borovin/block) [![Coverage Status](https://coveralls.io/repos/borovin/block/badge.svg?branch=master&service=github)](https://coveralls.io/github/borovin/block?branch=master)
+
+# Install
+
+```
+bower install borovin/block --save
+```
+
+# Use
 
 
 Extended Backbone View
+
+You can extend any Block class by static Block.extend method. This method set all passed properties to new block prototype using
+deep merge method.
+
+```
+var SomeBlock = Block.extend({a: {b: 1}}, {a: {c: 2}});
+
+var someBlock = new SomeBlock();
+
+someBlock.a.b === 1;
+someBlock.a.c === 2;
+```
 
 ## Constructor
 
 `new Block(config)`
 
 Block can be initialized as regular function without new keyword.
+
+```
+var block = Block(config);
+```
+
+All config properties passed to constructor will be set on block instance using deep merge algorithm.
+
+```
+var SomeBlock = Block.extend({a: {b: 1}});
+var someBlock = new SomeBlock({a: {c: 2}});
+someBlock.a.b === 1;
+someBlock.a.c === 2;
+```
 
 | Param | Type | Description |
 | ----- | -----| ----------- |
@@ -27,6 +59,7 @@ Block can be initialized as regular function without new keyword.
 `function`
 
 Block element for initialization. It will be replaced with block.template during render process.
+May be an existing DOM element or css selector.
 
 ```
 new Block({
@@ -40,17 +73,219 @@ new Block({
 
 
 
+### events
+`object`
+
+Same as Backbone.View events. Delegate event listeners to block.el.
+
+```
+var SomeBlock = Block.extend({
+     events: {
+         'initialized .childBlock': function(e, childBlock){}
+     }
+});
+```
+
+
+
+### globalEvents
+`object`
+
+Same as events, but delegated to document. By global events independent block (not parent->child) can listen events from each other.
+
+```
+var SomeBlock = Block.extend({
+     globalEvents: {
+         'initialized .otherBlock': function(e, otherBlock){}
+     }
+});
+```
+
+
+
+### template
+`string`
+`function`
+
+Result of template function will replace block.el during render process.
+You can use any template engine. All block properties are available throw this context inside template function.
+
+```
+var SomeBlock = Block.extend({
+     template: function(){
+         return '<div>' + this.anyProperty + '</div>'
+     }
+});
+```
+
+
+
+### attributes
+`function`
+`object`
+
+Map (or function that returns map) of html attributes which will be added to block.el during render process.
+
+```
+var block = new Block({
+     project: 'reltio',
+     attributes: function(){
+         return {
+             href: 'http://' + this.project + '.com',
+             title: this.project + ' web site'
+         }
+     }
+});
+```
+
+
+
+### className
+`function`
+`string`
+
+String (or function that return string) which will be added to block.el class during render process.
+
+```
+var block = new Block({
+     status: 'active',
+     className: function(){
+         return 'listItem ' + this.status;
+     }
+})
+```
+
+
+
+### id
+`function`
+`string`
+
+String (or function that return string) which will be set as block.el id during render process.
+
+
+
 
 ## Methods
 
 
 
 ### render(data)
-Render method description
+Render method set new data on block instance and invoke template function if it is defined. Current block
 
 | Param | Type | Description |
 | ----- | -----| ----------- |
 | data | `object` | data which will be set to block instance before rendering. So you can get it in template throw this context. |
+
+
+
+
+### get()
+You can get any block property by keypath. If one of the property is undefined get method return undefined (not error).
+If one of the property is function it will be executed in block context and return the result to get method.
+
+```
+var block = new Block({
+     text: 'string',
+     a: {
+         b: 2,
+         c: function(){ return this.text; }
+     }
+});
+
+block.get('a.b') // -> 2
+block.get('a.c') // -> 'string'
+block.get('a.b.c.d') // -> undefined
+```
+
+
+
+### set()
+Update or create property by keypath. If some property in this path does not exist it will be created automatically.
+`set` method return only changed properties. All changed properties trigger change:propertyKeyPath event.
+
+```
+var block = new Block({
+     a: {
+         b: 1
+     }
+});
+
+block.on('change:a.b', function(e, newValue){
+     console.log(newValue) // -> 2
+});
+
+block.on('change:a', function(e, newValue){
+     console.log(newValue) // -> {b: 2}
+});
+
+block.set('a.b', 2);
+```
+
+
+
+### include(constructor, params)
+`include` method is proper way to decompose block into sub-blocks in template. It will automatically initialize and remove all nested
+blocks during render process to avoid memory leaks.
+
+```
+var block = new Block({
+     template: function() {
+         return '<div>' + this.include(childBlock, {text: 'child'}) + '</div>'
+     }
+});
+```
+
+| Param | Type | Description |
+| ----- | -----| ----------- |
+| constructor |  |  |
+
+| params |  |  |
+
+
+
+
+### initBlock(Child, params)
+If you need to initialize child block manually (not inside template) you can use this method.
+All child blocks will be properly removed when you remove parent block to avoid memory leaks.
+
+```
+var parentBlock = new Block();
+var childBlock = parentBlock.initBlock(Block, {text: 'child'});
+parentBlock.remove();
+```
+
+| Param | Type | Description |
+| ----- | -----| ----------- |
+| Child |  |  |
+
+| params |  |  |
+
+
+
+
+### remove()
+Removes block and all it's children. Clear all event listeners.
+
+
+
+### trigger(event, data)
+Trigger event with any data as event properties. Event will be triggered by block instance and block.el as well.
+
+```
+var block = new Block();
+
+block.on('someEvent', function(data){});
+block.$el.on('someEvent', function(e, data){});
+
+block.trigger('someEvent');
+```
+
+| Param | Type | Description |
+| ----- | -----| ----------- |
+| event |  |  |
+
+| data |  |  |
 
 
 
@@ -62,6 +297,10 @@ Render method description
 ### initializing
 initializing event triggers before initialize and render methods
 
+```
+block.on('initializing', function(block){});
+```
+
 | Property | Type | Description |
 | ----- | -----| ----------- |
 | block | `object` | block instance where the event was appeared |
@@ -71,6 +310,10 @@ initializing event triggers before initialize and render methods
 ### initialized
 initialized event triggers after initialize and render methods
 
+```
+block.on('initialized', function(block){});
+```
+
 | Property | Type | Description |
 | ----- | -----| ----------- |
 | block | `object` | block instance where the event was appeared |
@@ -81,3 +324,15 @@ initialized event triggers after initialize and render methods
 
 
 
+
+# Test
+
+```
+npm test
+```
+
+# Generate documentation
+
+```
+npm run docs
+```
--- index.js
+++ index.js
@@ -10,6 +10,18 @@ var View = backbone.View;
 /**
  * Extended Backbone View
  *
+ * You can extend any Block class by static Block.extend method. This method set all passed properties to new block prototype using
+ * deep merge method.
+ *
+ * ```
+ * var SomeBlock = Block.extend({a: {b: 1}}, {a: {c: 2}});
+ *
+ * var someBlock = new SomeBlock();
+ *
+ * someBlock.a.b === 1;
+ * someBlock.a.c === 2;
+ * ```
+ *
  * @module block
  */
 
@@ -17,6 +29,19 @@ module.exports = createClass(View, {
 
     /**
      * Block can be initialized as regular function without new keyword.
+     *
+     * ```
+     * var block = Block(config);
+     * ```
+     *
+     * All config properties passed to constructor will be set on block instance using deep merge algorithm.
+     *
+     * ```
+     * var SomeBlock = Block.extend({a: {b: 1}});
+     * var someBlock = new SomeBlock({a: {c: 2}});
+     * someBlock.a.b === 1;
+     * someBlock.a.c === 2;
+     * ```
      *
      * @constructor Block
      * @param {object} config all properties from this config-object will be set on block instance
@@ -27,12 +52,16 @@ module.exports = createClass(View, {
 
         block.cid = _.uniqueId('block');
 
-        set(block, block.defaults, config);
+        set(block, config);
 
         block._initElement();
 
         /**
          * initializing event triggers before initialize and render methods
+         *
+         * ```
+         * block.on('initializing', function(block){});
+         * ```
          *
          * @event initializing
          * @property {object} block block instance where the event was appeared
@@ -46,6 +75,10 @@ module.exports = createClass(View, {
             /**
              * initialized event triggers after initialize and render methods
              *
+             * ```
+             * block.on('initialized', function(block){});
+             * ```
+             *
              * @event initialized
              * @property {object} block block instance where the event was appeared
              */
@@ -55,6 +88,7 @@ module.exports = createClass(View, {
 
     /**
      * Block element for initialization. It will be replaced with block.template during render process.
+     * May be an existing DOM element or css selector.
      *
      * ```
      * new Block({
@@ -69,18 +103,102 @@ module.exports = createClass(View, {
      * @type {HTMLElement|selector|string|function}
      */
     el: '<div></div>',
-    globalEvents: {},
+
+    /**
+     * Same as Backbone.View events. Delegate event listeners to block.el.
+     *
+     * ```
+     * var SomeBlock = Block.extend({
+     *      events: {
+     *          'initialized .childBlock': function(e, childBlock){}
+     *      }
+     * });
+     * ```
+     *
+     * @type {object}
+     */
     events: {},
-    defaults: {},
+
+    /**
+     * Same as events, but delegated to document. By global events independent block (not parent->child) can listen events from each other.
+     *
+     * ```
+     * var SomeBlock = Block.extend({
+     *      globalEvents: {
+     *          'initialized .otherBlock': function(e, otherBlock){}
+     *      }
+     * });
+     * ```
+     *
+     * @type {object}
+     */
+    globalEvents: {},
+
+    /**
+     * Result of template function will replace block.el during render process.
+     * You can use any template engine. All block properties are available throw this context inside template function.
+     *
+     * ```
+     * var SomeBlock = Block.extend({
+     *      template: function(){
+     *          return '<div>' + this.anyProperty + '</div>'
+     *      }
+     * });
+     * ```
+     *
+     * @type {string|function}
+     */
     template: null,
+
+    /**
+     * Map (or function that returns map) of html attributes which will be added to block.el during render process.
+     *
+     * ```
+     * var block = new Block({
+     *      project: 'reltio',
+     *      attributes: function(){
+     *          return {
+     *              href: 'http://' + this.project + '.com',
+     *              title: this.project + ' web site'
+     *          }
+     *      }
+     * });
+     * ```
+     *
+     * @type {function|object}
+     */
+    attributes: {},
+
+    /**
+     * String (or function that return string) which will be added to block.el class during render process.
+     *
+     * ```
+     * var block = new Block({
+     *      status: 'active',
+     *      className: function(){
+     *          return 'listItem ' + this.status;
+     *      }
+     * })
+     * ```
+     *
+     * @type {function|string}
+     */
+    className: '',
+
+    /**
+     * String (or function that return string) which will be set as block.el id during render process.
+     *
+     * @type {function|string}
+     */
+    id: null,
 
     _children: {},
 
     /**
-     * Render method description
+     * Render method set new data on block instance and invoke template function if it is defined. Current block
      *
      * @param {object} data data which will be set to block instance before rendering. So you can get it in template throw this context.
-     * @return {object} block instance for chaining
+     * @returns {object} block instance for chaining
      */
     render: function(data) {
 
@@ -93,27 +211,41 @@ module.exports = createClass(View, {
             id = block.get('id'),
             $oldEl = block.$el;
 
-        block.undelegateEvents();
-        block._removeBlocks();
-
         if (id) {
             attrs.id = id;
         }
 
         if (block.template) {
-            $oldEl.replaceWith(block._initElement(block.template()));
+            $oldEl.replaceWith(block._initElement(block.get('template')));
         }
 
         block.$el
             .attr(attrs)
             .addClass(block.get('className'));
 
-        block.delegateEvents();
-        block._initBlocks();
-
         return block;
     },
 
+    /**
+     * You can get any block property by keypath. If one of the property is undefined get method return undefined (not error).
+     * If one of the property is function it will be executed in block context and return the result to get method.
+     *
+     * ```
+     * var block = new Block({
+     *      text: 'string',
+     *      a: {
+     *          b: 2,
+     *          c: function(){ return this.text; }
+     *      }
+     * });
+     *
+     * block.get('a.b') // -> 2
+     * block.get('a.c') // -> 'string'
+     * block.get('a.b.c.d') // -> undefined
+     * ```
+     *
+     * @returns {value|undefined}
+     */
     get: function() {
 
         var block = this,
@@ -122,6 +254,30 @@ module.exports = createClass(View, {
         return get.apply(null, args);
     },
 
+    /**
+     * Update or create property by keypath. If some property in this path does not exist it will be created automatically.
+     * `set` method return only changed properties. All changed properties trigger change:propertyKeyPath event.
+     *
+     * ```
+     * var block = new Block({
+     *      a: {
+     *          b: 1
+     *      }
+     * });
+     *
+     * block.on('change:a.b', function(e, newValue){
+     *      console.log(newValue) // -> 2
+     * });
+     *
+     * block.on('change:a', function(e, newValue){
+     *      console.log(newValue) // -> {b: 2}
+     * });
+     *
+     * block.set('a.b', 2);
+     * ```
+     *
+     * @returns {object|undefined}
+     */
     set: function() {
 
         var block = this,
@@ -144,6 +300,22 @@ module.exports = createClass(View, {
         return changed;
     },
 
+    /**
+     * `include` method is proper way to decompose block into sub-blocks in template. It will automatically initialize and remove all nested
+     * blocks during render process to avoid memory leaks.
+     *
+     * ```
+     * var block = new Block({
+     *      template: function() {
+     *          return '<div>' + this.include(childBlock, {text: 'child'}) + '</div>'
+     *      }
+     * });
+     * ```
+     *
+     * @param constructor
+     * @param params
+     * @returns {string}
+     */
     include: function(constructor, params) {
 
         var block = this,
@@ -168,6 +340,20 @@ module.exports = createClass(View, {
         return include;
     },
 
+    /**
+     * If you need to initialize child block manually (not inside template) you can use this method.
+     * All child blocks will be properly removed when you remove parent block to avoid memory leaks.
+     *
+     * ```
+     * var parentBlock = new Block();
+     * var childBlock = parentBlock.initBlock(Block, {text: 'child'});
+     * parentBlock.remove();
+     * ```
+     *
+     * @param Child
+     * @param params
+     * @returns {childBlock}
+     */
     initBlock: function(Child, params) {
 
         var block = this;
@@ -182,6 +368,9 @@ module.exports = createClass(View, {
         return Child;
     },
 
+    /**
+     * Removes block and all it's children. Clear all event listeners.
+     */
     remove: function() {
 
         var block = this;
@@ -199,7 +388,22 @@ module.exports = createClass(View, {
         View.prototype.remove.apply(block, arguments);
     },
 
-    trigger: function(event) {
+    /**
+     * Trigger event with any data as event properties. Event will be triggered by block instance and block.el as well.
+     *
+     * ```
+     * var block = new Block();
+     *
+     * block.on('someEvent', function(data){});
+     * block.$el.on('someEvent', function(e, data){});
+     *
+     * block.trigger('someEvent');
+     * ```
+     *
+     * @param event
+     * @param data
+     */
+    trigger: function(event, data) {
 
         var block = this;
 
@@ -241,11 +445,19 @@ module.exports = createClass(View, {
 
     _initElement: function(el) {
 
-        this.el = $(el || this.get('el'))[0];
-        this.$el = $(this.el);
-        this.el.block = this;
+        var block = this;
 
-        return this.el;
+        block.undelegateEvents();
+        block._removeBlocks();
+
+        block.el = $(el || block.get('el'))[0];
+        block.$el = $(block.el);
+        block.el.block = block;
+
+        block.delegateEvents();
+        block._initBlocks();
+
+        return block.el;
 
     },
 
