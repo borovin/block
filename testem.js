0f9e6222c0ab3ddd69c9ba08cbba821c9ec5c152
Andrew Borovin <a@borovin.com>
2016-02-13 22:56:28 +0300

    run ci tests only on phantomjs

--- testem.js
+++ testem.js
@@ -3,5 +3,6 @@
 module.exports = {
     src_files: ['tests/*', 'block.js'],
     serve_files: 'tests/*',
-    test_page: 'tools/testem.mustache'
+    test_page: 'tools/testem.mustache',
+    launch_in_ci: ['PhantomJS']
 };
