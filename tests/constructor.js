f24f574ecb51fd1323f77a6a3c46454a026d9479
Andrew Borovin <a@borovin.com>
2016-02-18 03:37:17 +0300

    add tests

--- tests/constructor.js
+++ tests/constructor.js
@@ -10,6 +10,14 @@ describe('constructor', function () {
 
     });
 
+    it('should return view instance without new keyword', function () {
+
+        var view = View();
+
+        expect(view instanceof View).toBe(true);
+
+    });
+
     it('should set all props to instance', function () {
 
         var view = new View({
@@ -19,6 +27,20 @@ describe('constructor', function () {
 
         expect(view.a).toBe(1);
         expect(view.b).toBe(2);
+
+    });
+
+    it('should set nested props to instance', function () {
+
+        var view = new View({
+            a: {
+                b: {
+                    c: 1
+                }
+            }
+        });
+
+        expect(view.a.b.c).toBe(1);
 
     });
 });
