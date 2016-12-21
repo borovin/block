9c24cc91a4099068fd226da1d249c18dc2762f11
Andrew Borovin <a@borovin.com>
2016-12-21 23:02:30 +0300

    update tests

--- __tests__/button.js
+++ __tests__/button.js
@@ -1,12 +1,9 @@
 const Button = require('../button');
 
-require('@webcomponents/custom-elements');
-
 it('Should render content', () => {
     const button = new Button();
 
     button.connectedCallback();
 
     expect(button.innerHTML).toBe('button');
-
 });
