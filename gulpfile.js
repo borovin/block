6b36e8dbbcbf366c264a7e90d772d36e00f3a48d
Andrew Borovin <a@borovin.com>
2017-05-07 16:34:37 +0300

    deps cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,21 @@ const rename = require('gulp-rename');
 const rollup = require('rollup');
 const commonjs = require('rollup-plugin-commonjs');
 const nodeResolve = require('rollup-plugin-node-resolve');
-const babili = require('rollup-plugin-babili');
 const imagemin = require('gulp-imagemin');
 const capitalize = require('lodash/capitalize');
 const csso = require('gulp-csso');
 const path = require('path');
 const packageJSON = require('./package.json');
 const gulpif = require('gulp-if');
-const {argv} = require('yargs');
 const babel = require('rollup-plugin-babel');
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 gulp.task('default', ['rollup']);
 
 gulp.task('styles', () => {
     return gulp.src('src/**/*.css')
-        .pipe(gulpif(argv.production, csso()))
+        .pipe(gulpif(isProduction, csso()))
         .pipe(wrap((data) => {
             const appendStylesPath = path.relative(path.dirname(data.file.path), path.join(__dirname, '/src/utils/appendStyles.js'));
             const stylesPath = path.relative(__dirname, data.file.path);
@@ -36,20 +36,12 @@ appendStyles(\`<style id="${stylesID}">${data.contents}</style>\`);`;
 
 gulp.task('icons', () => {
     return gulp.src('src/icons/**/*.svg')
-        .pipe(gulpif(argv.production, imagemin()))
+        .pipe(gulpif(isProduction, imagemin()))
         .pipe(wrap('module.exports = `<%= contents %>`'))
         .pipe(rename({
             extname: '.js'
         }))
         .pipe(gulp.dest('src/icons'));
-});
-
-gulp.task('cover', function() {
-    return gulp.src(['src/**/*.js'])
-    // Covering files
-        .pipe(istanbul())
-        // Force `require` to return covered files
-        .pipe(istanbul.hookRequire());
 });
 
 gulp.task('rollup', ['icons', 'styles'], () => {
@@ -59,30 +51,13 @@ gulp.task('rollup', ['icons', 'styles'], () => {
         'form'
     ];
 
-    function getPlugins() {
-        const plugins = [
-            nodeResolve(),
-            commonjs()
-        ];
-
-        if (argv.production) {
-            plugins.push(babili({
-                comments: false
-            }));
-        }
-
-        if (argv.coverage) {
-            plugins.push(babel({
-                plugins: ["istanbul"]
-            }));
-        }
-
-        return plugins;
-    }
-
     return Promise.all(blocks.map(blockName => rollup.rollup({
         entry: `./src/${blockName}/index.js`,
-        plugins: getPlugins()
+        plugins: [
+            nodeResolve(),
+            commonjs(),
+            babel()
+        ]
     }).then(bundle => bundle.write({
         format: 'umd',
         moduleName: `Block.${capitalize(blockName)}`,
