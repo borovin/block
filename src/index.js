d580bfd6be71bacd28a79e1ac7fcaed27fb5ca0a
Andrew Borovin <a@borovin.com>
2017-05-08 01:58:30 +0300

    ignore index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-//this file was generated automatically. Do not edit it manually.
-module.exports = {
-'button': require('./button'),
-'form': require('./form'),
-'input': require('./input'),
-'input-checkbox': require('./input-checkbox'),
-'input-radio': require('./input-radio'),
-'input-text': require('./input-text'),
-};
